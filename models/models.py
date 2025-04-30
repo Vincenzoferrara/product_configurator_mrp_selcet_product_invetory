e629145c007b383409a10ed96d805129d5dd07d1
Vincenzoferrara <vincy.fer2001@gmail.com>
2025-04-30 18:59:39 +0200

    rimossi riferimenti diretti perche non voglio modoficare il addon principale

--- models/models.py
+++ models/models.py
@@ -1,16 +1,14 @@
 from odoo import models, fields, api
 
-class ProductConfiguratorMrp(models.Model):
-    _name = 'product.configurator.mrp'
-    _description = 'Configurazione MRP'
+class ProductConfiguratorMrpSelectProductInventory(models.Model):
+    _name = 'product.configurator.mrp.select.product.inventory'
+    _description = 'Prodotti dal Magazzino per Configurazione MRP'
 
-    product_template_id = fields.Many2one('product.template', string='Modello Prodotto', required=True)
-    product_qty_ids = fields.One2many('product.configurator.mrp.qty', 'config_id', string='Prodotti e Quantità')
-
-class ProductConfiguratorMrpQty(models.Model):
-    _name = 'product.configurator.mrp.qty'
-    _description = 'Quantità Prodotto Configurazione MRP'
-
+    config_id = fields.Many2one('product.configurator.mrp', string='Configurazione', required=True, ondelete='cascade')
     product_id = fields.Many2one('product.product', string='Prodotto', required=True)
     qty = fields.Float(string='Quantità', required=True)
-    config_id = fields.Many2one('product.configurator.mrp', string='Configurazione', required=True, ondelete='cascade')
+
+class ProductConfiguratorMrp(models.Model):
+    _inherit = 'product.configurator.mrp'
+
+    product_inventory_ids = fields.One2many('product.configurator.mrp.select.product.inventory', 'config_id', string='Prodotti dal Magazzino')
